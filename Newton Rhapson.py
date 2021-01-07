49c55e298282b2c2830ffb96562d76c594340ffe
Jithin Johnson <76789703+jithu7432@users.noreply.github.com>
2021-01-07 11:07:39 +0530

    Create Newton Rhapson.py

---
+++ Newton Rhapson.py
@@ -0,0 +1,26 @@
+#JJ
+from pandas import DataFrame
+
+
+def f(x):
+    return x ** 3 - 8 * x + 3
+
+
+def df(x):
+    return (f(x + h) - f(x)) / h
+
+
+fl, dfl, xl, xll = ([] for _ in range(4))
+epsilon = 1e-10
+h = 1e-10
+x = float(input("Enter initial guess: "))
+while True:
+    xl.append(x)
+    x = x - f(x) / df(x)
+    xll.append(x),fl.append(f(x)),dfl.append(df(x))
+    if abs(f(x)) <= epsilon:
+        break
+data = {"X": xl, "f(x)": fl, "f'(x)": dfl, "Xn": xll}
+frame = DataFrame(data)
+print(frame)
+print("The root of the equation is:", x)
